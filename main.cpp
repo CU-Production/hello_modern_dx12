54aada45d5a1df12dbdeaa17cb190405c34ae897
THISISAGOODNAME <yangyj19931231@gmail.com>
2025-02-21 17:05:32 +0800

    pch

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
-#include "D3D12Lite.h"
 #include "Shaders/Shared.h"
-#include "imgui/imgui.h"
 #include "imgui/imgui_impl_win32.h"
 #include "imgui/imgui_impl_dx12.h"
 
---
+++ stdafx.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <d3d12.h>
+#include <imgui/imgui.h>
+#include "d3d12lite.h"
